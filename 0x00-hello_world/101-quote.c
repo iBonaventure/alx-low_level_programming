29da564e222e11316af1de4892135984b7f228d3
iBonaventure <chigbobonaventure@gmail.com>
2023-03-17 02:55:49 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,12 @@
 #include <unistd.h>
 /**
  * main - A c program that prints a line to the standard error
- * Return 1 (Success)
+ * Return: 1 (Success)
  *
  */
 
 int main(void)
 {
-        fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-        return (1);
+write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
