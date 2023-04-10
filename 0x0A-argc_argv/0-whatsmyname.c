9b7fd4b5da5da9ef0f7b00abc10671b93fbaddbb
iBonaventure <chigbobonaventure@gmail.com>
2023-04-10 07:19:54 -0700

    Task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,14 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * main - prints the name of the program
  * @argc: number of arguments
  * @argv: array of arguments
- * unused_arg is intentionally unused because it's only needed in certain cases
+ *
  * Return: 0 (Success)
  */
 
-int main(int argc _attribute_((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
 	return (0);
