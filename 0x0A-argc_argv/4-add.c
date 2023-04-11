5d54f6f08ce462a9ecb42a66d3993a3e79919392
iBonaventure <chigbobonaventure@gmail.com>
2023-04-11 12:22:27 -0700

    Task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -4,59 +4,28 @@
 #include <string.h>
 
 /**
- * check_num - Check - string there are digit
- * @str: arrayv str
- * Return: Always 0
- */
-
-int check_num(char *str)
-{
-	/*Declareing variables*/
-	unsigned int count;
-
-	count = 0;
-	while (count < strlen(str)) /*This counts strings*/
-	{
-		if (!isdigit(str[count])) /*This checks if str there are digit*/
-		{
-			return (0);
-		}
-		count++;
-	}
-	return (1);
-}
-
-/**
- * main - This prints the name of the program
- * @argc: This counts arguments
- * @argv: Arguments
- * Return: 0
+ * main - This adds positive numbers
+ * @argc: number of command line arguments
+ * @argv: The array that contains the command line arguments
+ * Return: 0 when successful
  */
 
 int main(int argc, char *argv[])
 {
-	/*Declaring variables*/
-	int count;
-	int str_to_int;
-	int sum = 0;
-}
-	count = 1;
-	while (count < argc) /*Goes through the whole array*/
-{
-	if (check_num(argv[count]))
-	{
-		str_to_int = atoi(argv[count]); /*ATOI --> converts string to *int*/
-		sum += str_to_int;
-	}
-	/*condition if one of the nums contains symbols that arent digits*/
-	else
-	{
-		printf("Error\n");
-		return (1);
-	}
-	count++;
-}
-printf("%d\n", sum); /*This prints sum*/
-return (0);
-}
+	int i, j, add = 0;
 
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
