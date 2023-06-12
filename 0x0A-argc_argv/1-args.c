5838db5ad04c60a21b69d0cf8eb70b54493e35d0
iBonaventure <chigbobonaventure@gmail.com>
2023-06-11 18:11:39 -0700

    Task 0-5

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the number of arguments passed
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv; /*ignore argv*/
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
